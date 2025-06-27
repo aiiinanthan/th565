983042738cac0c66840d80fa5330645819f1dd5d
me22367889 <sphaventhan@gmail.com>
2025-06-27 07:55:57 +0530

    Initial project upload

---
+++ src/App.tsx
@@ -0,0 +1,192 @@
+
+import"./App.css";
+import Sec from"./assets/Sec.jpg";
+import mo from"./assets/mo.mp4";
+import ee from"./assets/ee.mp4";
+
+function App() {
+  return (
+    <div className="overall">
+    <div>
+      <title>The project center</title>
+      <div className="blockbox">
+      <h1 className="heading"> PROJECT PLACE</h1>
+      </div>
+      <p className="st">Bachular of computerscience</p>
+      <p className="so">Free idea</p>
+      <h2 className="yu">the</h2>
+
+      <div className="over">
+        <h3 className="tr">
+        BSC - computerscience</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={ee} type="
+            video/mp4"/>
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      </div>
+      <div className="over">
+        <h3 className="tr">
+        BSC</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={ee} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      <div className="over">
+        <h3 className="tr">
+        BSC</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={mo} type="
+            the" />
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      <div className="over">
+        <h3 className="tr">
+        BSC</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={mo} type="
+            the" />
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      <div className="over">
+        <h3 className="tr">
+        BSC</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={mo} type="
+            the" />
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      <div className="over">
+        <h3 className="tr">
+        BSC</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={mo} type="
+            the" />
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      <div className="over">
+        <h3 className="tr">
+        BSC</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={mo} type="
+            the" />
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      <div className="over">
+        <h3 className="tr">
+        BSC</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={mo} type="
+            the" />
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      <div className="over">
+        <h3 className="tr">
+        BSC</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={mo} type="
+            the" />
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      <div className="over">
+        <h3 className="tr">
+        BSC</h3>
+      <div className="content">
+        <img src={Sec} alt="thee" className="leftside">
+        </img>
+        <div className="video">
+          <video width={600} controls>
+            <source src={mo} type="
+            the" />
+          </video>
+        </div>
+      </div>
+      <p className="yyy">ABSTRACT :</p>
+      <p className="uuu"> this is the main nabstract and its was the main thing of it ansd then universasl of it the main thing this is the project sos tits thign a main conecern a qulaity a secirew </p>
+      <p className="yyy">VIVA</p>      
+      </div>
+      </div>
+  )
+}
+export default App;
